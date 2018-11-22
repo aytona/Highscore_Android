c819377b0391d029b223163531d338708797c841
aytona <christopher.aytona@gmail.com>
2018-11-22 10:46:24 -0500

    Score uml outline, except need to remove toString

--- app/src/main/java/com/example/vanessali/highscores_li/model/Score.java
+++ app/src/main/java/com/example/vanessali/highscores_li/model/Score.java
@@ -1,15 +1,13 @@
 package com.example.vanessali.highscores_li.model;
 
-public class Score {
+import android.os.Parcel;
+import android.os.Parcelable;
 
-    private String name;
-    private int number;
+public class Score {
 
     public Score(String userName ,int userNumber){ //if using to string you need to add display string in the paramters
         name = userName;
-        number = userNumber;
-
-
+        score = userNumber;
     }
 
     public Score(String string){
@@ -17,22 +15,51 @@ public class Score {
         String[] scoreAttributes = string.split(",");
 
         //sets the attributes
-        number =Integer.parseInt(scoreAttributes[0]);//
+        score =Integer.parseInt(scoreAttributes[0]);//
         name = scoreAttributes[1];
+    }
 
+    private Score(Parcel p) {
 
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int n_score) {
+        score = n_score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String n_Name) {
+        name = n_Name;
+    }
+
+    public int compareTo(Score score) {
+        return 0;   // TODO
+    }
+
+    public int describeContents() {
+        return 0; // TODO Parcelable
     }
 
     public String toFile(){
-        return String.format("%d,%s", number, name );
+        return String.format("%d,%s", score, name );
     }
 
+    private String name;
+    private int score;
 
+    // TODO: Remove
     @Override
     public String toString() {
         return "Score{" +
                 "name='" + name + '\'' +
-                ", number=" + number +
+                ", number=" + score +
                 '}';
     }
 }
