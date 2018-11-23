831b8a8f7da54560c8e957f0a4fd4e8236066b93
aytona <christopher.aytona@gmail.com>
2018-11-23 18:53:18 -0500

    Used other constructor

--- app/src/main/java/com/example/vanessali/highscores_li/MainActivity.java
+++ app/src/main/java/com/example/vanessali/highscores_li/MainActivity.java
@@ -168,8 +168,7 @@ public class MainActivity extends Activity {
 
                 while (scanner.hasNextLine()) {//Keep reading the file as long as there's data
                     String line = scanner.nextLine();
-                    String[] data = line.split(":");
-                    Score newScore = new Score(data[0], Integer.valueOf(data[1]));
+                    Score newScore = new Score(line);
                     scoreList.add(newScore);
                 }
                 scanner.close();//close scanner = close fis
